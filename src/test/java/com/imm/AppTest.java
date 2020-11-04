7816bc878d37c599c84a69343450e6be59bc5af3
Ai-MM <immgo@qq.com>
2020-11-04 18:42:40 +0800

    Initial commit

---
+++ src/test/java/com/imm/AppTest.java
@@ -0,0 +1,20 @@
+package com.imm;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("pass");
+    }
+}
