81a44356b81f21109dfab481e36e1769205b72ea
Ai-MM <immgo@qq.com>
2020-11-12 12:57:27 +0800

    7

--- src/test/java/com/imm/WebTest.java
+++ src/test/java/com/imm/WebTest.java
@@ -1,6 +1,9 @@
 package com.imm;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -13,15 +16,27 @@ import java.util.concurrent.TimeUnit;
  * @Date: 2020-11-11 20:30
  */
 public class WebTest {
+    static WebDriver driver;
+
+    @BeforeAll
+    static void beforeAll() {
+//        System.setProperty("webdriver.edge.driver", "E:\\Files\\WebDriver\\msedgedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
     @Test
     void webTest() {
-//        System.setProperty("webdriver.edge.driver", "E:\\Files\\WebDriver\\msedgedriver.exe");
-        WebDriver driver = new EdgeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://ceshiren.com/");
+        driver.findElement(By.cssSelector("#search-button > svg")).click();
+    }
+
+    @AfterAll
+    static void afterAll() {
         try {
             Thread.sleep(2000);
-        } catch (InterruptedException e) {
+        } catch (
+                InterruptedException e) {
             e.printStackTrace();
         }
         driver.quit();
