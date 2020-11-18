688e26ff71facfbc324a3da9255b08e67eb59a0c
Ai-MM <immgo@qq.com>
2020-11-18 21:50:09 +0800

    天é首次添加appium

---
+++ src/main/java/com/imm/framework/app/AppBasePage.java
@@ -0,0 +1,50 @@
+package com.imm.framework.app;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class AppBasePage {
+    public AppiumDriver<MobileElement> driver;
+    public WebDriverWait wait;
+
+    public AppBasePage() {
+    }
+
+    public AppBasePage(AppiumDriver<MobileElement> driver) {
+        this.driver = driver;
+    }
+
+    public AppBasePage(String packageName, String activityName) {
+        startApp(packageName, activityName);
+    }
+
+    public void startApp(String packageName, String activityName) {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "android");
+        desiredCapabilities.setCapability("udid", "emulator-5554"); //设置唯一标识符
+        desiredCapabilities.setCapability("deviceName", "phone");
+//        desiredCapabilities.setCapability("avd", "Pixel_2_API_29"); //启动模拟器
+        desiredCapabilities.setCapability("appPackage", packageName);
+        desiredCapabilities.setCapability("appActivity", activityName);
+        desiredCapabilities.setCapability("unicodeKeyboard", "true");
+        desiredCapabilities.setCapability("resetKeyboard", "true");
+        desiredCapabilities.setCapability("dontStopAppOnReset", "true"); //不重启app
+        desiredCapabilities.setCapability("skipLogcatCapture", "true"); //跳过日志记录
+        //高危操作，设置错误会清空聊天记录
+        desiredCapabilities.setCapability("noReset", "true");
+        try {
+            driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 10);
+    }
+}
--- src/test/java/com/imm/page/web/ContactPageTest.java
+++ src/test/java/com/imm/page/web/ContactPageTest.java
@@ -17,13 +17,14 @@ class ContactPageTest extends BaseTest {
 
     @ParameterizedTest
     @CsvSource({
-            "test1, test1, '11111111111'",
-            "test2, test2, '11111111112'",
-            "test3, test3, '11111111113'"
+            "test1, test1, 11111111111",
+            "test2, test2, 11111111112",
+            "test3, test3, 11111111113"
     })
     void addMemberTest(String userName, String acctId, String mobile) {
         contactPage.addMember(userName, acctId, mobile).search(userName);
         String actualAcctId = contactPage.getAcctId();
+        System.out.println(actualAcctId);
         contactPage.deleteMember();
         assertTrue(actualAcctId.contains(acctId));
     }
