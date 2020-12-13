b6aa380534e7858e7d0d5a5b55b911e38c20326d
Ai-MM <immgo@qq.com>
2020-12-13 15:56:48 +0800

    添加WireMock测试

--- src/main/java/com/imm/framework/page/BasePage.java
+++ src/main/java/com/imm/framework/page/BasePage.java
@@ -3,7 +3,6 @@ package com.imm.framework.page;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
---
+++ src/test/java/com/imm/mock/WireMockTest.java
@@ -0,0 +1,48 @@
+package com.imm.mock;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+
+/**
+ * @Author: iMM
+ * @Description:
+ * @Date: 2020/12/13
+ */
+public class WireMockTest {
+    static WireMockServer wireMockServer;
+
+    @BeforeAll
+    static void beforeAll() {
+        wireMockServer = new WireMockServer(options().port(8888));
+        wireMockServer.start();
+        configureFor(8888);
+    }
+
+    //    @Test
+    void demo1() throws InterruptedException {
+        stubFor(get(urlEqualTo("/c/job/31"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "text/plain;charset=UTF-8")
+                        .withBody("Hello World! Hello WireMockServer!测试")));
+        Thread.sleep(600000);
+    }
+
+    @Test
+    void demo2() throws IOException, InterruptedException {
+        stubFor(get(urlMatching(".*")).atPriority(10)
+                .willReturn(aResponse().proxiedFrom("https://ceshiren.com")
+                ));
+        stubFor(get(urlMatching("/c/job/31/l/latest.json.*")).atPriority(1)
+                .willReturn(aResponse()
+                                .withBody(Thread.currentThread().getContextClassLoader()
+                                        .getResourceAsStream("wiremock_response.json").readAllBytes())
+                ));
+        Thread.sleep(600000);
+    }
+}
